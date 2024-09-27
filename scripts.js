65c49212594ed48a9d77a1ebe0b8115fb6a4b94f
Miroslav99 <mbochko99@gmail.com>
2024-09-27 11:55:01 +0300

    Added logic to remove the last character and fixed bugs

--- scripts.js
+++ scripts.js
@@ -3,8 +3,12 @@ const operatorButton = document.querySelectorAll('.operate')
 const drawButton = document.getElementById('draw')
 const display = document.getElementById('display')
 const reset = document.getElementById('reset')
+const clear = document.getElementById('clear')
+const del = document.getElementById('del')
 
-
+function percentage (a, b) {
+    return (b / 100) * a
+}
 
 function add(a, b) {
     return a + b;
@@ -36,6 +40,8 @@ function calculate(operator, num1, num2) {
     let a = parseFloat(num1)
     let b = parseFloat(num2)
     switch (operator) {
+        case '%':
+            return percentage(a, b)
         case '+':
             return add(a, b);
         case '-':
@@ -56,8 +62,9 @@ numberButton.forEach(el => {
     el.addEventListener('click', () => {
         const value = el.value
          if(operand === '') {
-            num1 += value
-            display.textContent = num1
+             num1 += value
+             display.textContent = num1
+            
          } else {
             num2 += value
             display.textContent = num1 + ' ' + operand + ' ' + num2
@@ -77,9 +84,13 @@ operatorButton.forEach(el => {
 
 
 drawButton.addEventListener('click', () => {
+    if(num1 === '' || operand === '') {
+        display.textContent = ''
+        num1 = ''
+        return
+    }
     result = calculate(operand, num1, num2)
     display.textContent = result
-
     num1 = result
     num2 = ''
     operand = ''
@@ -91,4 +102,22 @@ num1 = '';
 num2 = '';
 operand = '';
 result = null;
+})
+
+clear.addEventListener('click', () => {
+    display.textContent = '0'
+    num1 = '';
+    num2 = '';
+    operand = '';
+    result = null;
+})
+
+del.addEventListener('click', () => {
+      if(operand === '') {
+       num1 = num1.slice(0, -1)
+       display.textContent = num1
+} else if (num2 !== '') {
+    num2 = num2.slice(0, -1)
+    display.textContent = num1 + ' ' + operand + ' ' + num2
+}
 })
